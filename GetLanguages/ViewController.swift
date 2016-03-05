b63b7124bea33afe819df47a8b8123ac31488081
Ryoichi Izumita <r.izumita@caph.jp>
2016-03-06 07:24:27 +0900

    Initial Commit

---
+++ GetLanguages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetLanguages
+//
+//  Created by 和泉田 領一 on 2016/03/06.
+//  Copyright © 2016年 CAPH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
